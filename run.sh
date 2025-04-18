d96341ca031c2e5c07cd72d2ba498da0b998152f
bhchen2001 <bhchen312551074.cs12@nycu.edu.tw>
2025-04-18 15:17:25 +0800

    feat: script for S008

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
-for fold in {1..4}; do
+for fold in {1..2}; do
     # python3 main.py --train_classifier --gpu 0 --run_id fisheye639_S003_fold${fold} --dataset ntu_rgbd_60_fold${fold} --model_version 'v3' --batch_size 4 --num_epochs 30 --num_workers 8 --learning_rate 1e-4 --weight_decay 1e-6 --optimizer ADAM
-    python3 main.py --train_classifier --gpu 0 --run_id fisheye639_S003_CV${fold} --dataset ntu_rgbd_60_CV${fold} --model_version 'v3' --batch_size 4 --num_epochs 30 --num_workers 8 --learning_rate 1e-4 --weight_decay 1e-6 --optimizer ADAM
-    # python3 main.py --test_classifier --gpu 0 --load_model fisheye639_S003_fold${fold} --dataset ntu_rgbd_60_fold${fold} --model_version 'v3' --batch_size 4 --num_workers 8
+    # python3 main.py --train_classifier --gpu 0 --run_id fisheye639_S003_CV${fold} --dataset ntu_rgbd_60_CV${fold} --model_version 'v3' --batch_size 4 --num_epochs 30 --num_workers 8 --learning_rate 1e-4 --weight_decay 1e-6 --optimizer ADAM
+    # python3 main.py --train_classifier --gpu 0 --run_id fisheye639_S003_ITEM --dataset ntu_rgbd_60 --model_version 'v3' --batch_size 4 --num_epochs 30 --num_workers 8 --learning_rate 1e-4 --weight_decay 1e-6 --optimizer ADAM
+    # python3 main.py --test_classifier --gpu 0 --load_model fisheye639_S003_CV --dataset ntu_rgbd_60_CV${fold} --model_version 'v3' --batch_size 4 --num_workers 8
+    # python3 main.py --test_classifier --gpu 0 --load_model fisheye639_S003_ITEM --dataset ntu_rgbd_60 --model_version 'v3' --batch_size 4 --num_workers 8
+    python3 main.py --train_classifier --gpu 0 --run_id fisheye639_S008_fold${fold} --dataset ntu_rgbd_60_fold${fold} --model_version 'v3' --batch_size 4 --num_epochs 30 --num_workers 8 --learning_rate 1e-4 --weight_decay 1e-6 --optimizer ADAM
+    # python3 main.py --test_classifier --gpu 0 --load_model fisheye639_S008_fold${fold} --dataset ntu_rgbd_60_fold${fold} --model_version 'v3' --batch_size 4 --num_workers 8
+    echo "Fold ${fold} done"
 done
